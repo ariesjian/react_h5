66846847e2e118ccfa611a85085db0e8dbd145b4
ariesjian <2890283899@qq.com>
2019-03-12 10:06:46 +0800

    暂存

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import styles from './index.less';
+
 class App extends Component {
+    constructor() {
+        super()
+    }
+
+    componentDidMount() {
+        const domObj = document.getElementsByTagName('*');
+        // 去重
+        const newObject = {};
+        const arr = [];
+        domObj.map(i => {
+            return {...i, name: i.name}
+        })
+        console.log()
+
+    }
+
     render() {
         return (
             <div className={styles.container}>
-                基础练习
+                <div>
+                    <span> 获取所有标签</span>
+                </div>
+                <p>
+                    <text>但是需要去掉重复的</text>
+                </p>
             </div>
+
         );
     }
 }
